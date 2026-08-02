52015734039e1e9e13104ee8631887b6f5189a10
Ammar Ansari <140953295+ammargit93@users.noreply.github.com>
2026-08-02 23:21:22 +0530

    Add: range-sum-query-2d-immutable - submission-2

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-2.py
@@ -0,0 +1,30 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        ROWS, COLS = len(matrix), len(matrix[0])
+        self.sum_mat = [[0]*(COLS+1) for _ in range(ROWS+1)]
+        for r in range(ROWS):
+            prefix = 0
+            for c in range(COLS):
+                prefix += matrix[r][c]
+                above = self.sum_mat[r][c+1]
+                self.sum_mat[r+1][c+1] = prefix+above
+        for r in self.sum_mat:
+            print(r)
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        r1, r2, c1, c2 = row1+1, row2+1, col1+1, col2+1
+        bottom_r = self.sum_mat[r2][c2]
+        print(bottom_r)
+        above = self.sum_mat[r1-1][c2]
+        print(above)
+        left = self.sum_mat[r2][c1-1]
+        print(left)
+        common = self.sum_mat[r1-1][c1-1]
+        print(common)
+        return bottom_r - above - left + common
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
